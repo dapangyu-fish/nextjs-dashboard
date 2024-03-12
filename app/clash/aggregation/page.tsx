3d399c081a4e68001e4dcc401fb4675bea668399
dapangyu-fish <2501808198@qq.com>
2024-03-12 13:59:21 +0800

    formate

--- app/clash/aggregation/page.tsx
+++ app/clash/aggregation/page.tsx
@@ -1,3 +1,11 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+
 export default function Page() {
-    return <p>clash aggregation</p>;
+    return (
+        <main className="flex min-h-screen flex-col p-6">
+            <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+                <AcmeLogo />
+            </div>
+        </main>
+    )
 }
